fea65c403d7d7ebd0d35810939233760113aea69
Garrett Gorman <garrettgorman1@gmail.com>
2024-08-13 14:44:09 -0400

    Preliminary structure and styling for confirmation page

--- client/src/pages/OrderConfirmation/OrderConfirmation.jsx
+++ client/src/pages/OrderConfirmation/OrderConfirmation.jsx
@@ -1,8 +1,9 @@
 import "./OrderConfirmation.css";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { BASE_URL } from "../../store/BASE_URL";
 import axios from "axios";
+import Placeholder from "../../assets/placeholder.png";
 
 function OrderConfirmation() {
   const [sessionData, setSessionData] = useState(null);
@@ -56,15 +57,125 @@ function OrderConfirmation() {
     };
     fetchMap();
   }, [loading]);
-  // console.log(map);
+  console.log(map);
   return (
-    <>
-      {!loading && <div className="beep">{sessionData.customerName}</div>}
-      <div>
-        <img src={map?.mapUrl} className="map" />
-      </div>
-    </>
+    <div className="confirmation-root">
+      {!loading && (
+        <>
+          <div className="confirmation-customer-section">
+            <div className="confirmation-header-wrapper">
+              <h4>Dimensional Glassworks</h4>
+              <div className="confirmation-header">
+                <span>
+                  <svg
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="check-mark"
+                  >
+                    <path d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z" />
+                  </svg>
+                </span>
+                <div className="confirmation-header-info">
+                  <p>Order #1234</p>
+                  <h5>Thank You {sessionData?.customerName.split(" ")[0]}!</h5>
+                </div>
+              </div>
+            </div>
+            <div className="confirmation-map-wrapper">
+              <img src={map?.mapUrl} className="confirmation-map" />
+              <h5>Your order is confirmed</h5>
+              <p>
+                We&#39;ve accepted your order, and we&#39;re getting it ready. A
+                confirmation email has been sent to{" "}
+                <span className="confirmation-email">{sessionData?.email}</span>
+                .
+              </p>
+            </div>
+            <div className="confirmation-customerInfo-container">
+              <h5>Customer Information</h5>
+              <div className="confirmation-customerInfo-wrapper">
+                <div className="confirmation-customerInfo-shipping-wrapper">
+                  <h6>Shipping Address</h6>
+                  <p>{sessionData.shippingAddress.name}</p>
+                  <p>{sessionData.shippingAddress.address.line1}</p>
+                  <p>{sessionData.shippingAddress.address.line2}</p>
+                  <p>
+                    {sessionData.shippingAddress.address.city},{" "}
+                    {sessionData.shippingAddress.address.state},{" "}
+                    {sessionData.shippingAddress.address.postal_code}
+                  </p>
+                  <p>United States</p>
+                  <p>{sessionData.phone}</p>
+                  <h6>Shipping Method</h6>
+                  <p>Express-Saver</p>
+                </div>
+                <div className="confirmation-customerInfo-billing-wrapper">
+                  <h6>Billing Address</h6>
+                  <p>{sessionData.billingAddress.name}</p>
+                  <p>{sessionData.billingAddress.address.line1}</p>
+                  <p>{sessionData.billingAddress.address.line2}</p>
+                  <p>
+                    {sessionData.billingAddress.address.city},{" "}
+                    {sessionData.billingAddress.address.state},{" "}
+                    {sessionData.billingAddress.address.postal_code}
+                  </p>
+                  <p>United States</p>
+                  <p>{sessionData.phone}</p>
+                  <h6>Payment Method</h6>
+                  <p>VISA ending in 4242</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="confirmation-orderInfo-container">
+            <div className="confirmation-orderInfo-items-wrapper">
+              {sessionData.orderItems.map((item) => (
+                <div key={item.id} className="confirmation-orderInfo-item">
+                  <img src={Placeholder} />
+                  <div>
+                    <h5>{item.name}</h5>
+                    <h6>${(item.price / 100).toFixed(2)}</h6>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="confirmation-orderInfo-subtotals">
+              <div>
+                <p>Subtotal</p>
+                <h6>10000</h6>
+              </div>
+              <div>
+                <p>Shipping</p>
+                <h6>${(sessionData.shippingCost / 100).toFixed(2)}</h6>
+              </div>
+            </div>
+            <div className="confirmation-orderInfo-total">
+              <p>Total</p>
+              <h6>
+                $
+                {(sessionData.orderTotal / 100).toLocaleString(undefined, {
+                  minimumFractionDigits: 2,
+                  maximumFractionDigits: 2,
+                })}
+              </h6>
+            </div>
+          </div>
+          <div className="confirmation-continue">
+            <Link to={"/premade"}>
+              <button>Continue Shopping</button>
+            </Link>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
 
+{
+  /* 
+  <div>
+        <img src={map?.mapUrl} className="map" />
+      </div>
+  */
+}
 export default OrderConfirmation;
--- server/mapQuest/index.js
+++ server/mapQuest/index.js
@@ -4,18 +4,19 @@ const axios = require("axios");
 router.get("/map/:address", async (req, res, next) => {
   try {
     const { address } = req.params;
-    console.log("Address", address);
+    // console.log("Address", address);
     const map = await axios.get(
       "https://www.mapquestapi.com/staticmap/v5/map",
       {
         params: {
           key: process.env.MAPQUEST_KEY,
           locations: address,
+          size: "300,200",
         },
       }
     );
     res.status(200).json({ mapUrl: map.request.res.responseUrl });
-    console.log(map.request.res.responseUrl);
+    // console.log(map.request.res.responseUrl);
   } catch (error) {
     console.error(error);
   }
